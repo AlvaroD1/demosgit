28f1d14becaf48080e12ec0e9c0c4dc5fbd531ed
AlvaroD1 <alvarodpr2@gmail.com>
2021-05-12 17:20:35 -0500

    Agregue el origen del Saludo

---
+++ src/ec/edu/utpl/poo/semana6/demo/Saludo.java
@@ -0,0 +1,8 @@
+package ec.edu.utpl.poo.semana6.demo;
+
+public class Saludo {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+        System.out.println("Desde Loja");
+    }
+}
